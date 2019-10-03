6ca31accd9c4887fb9db7a539b8a0bee5ee0e73e
Fabian Gutierrez <fagossa@gmail.com>
2019-10-03 21:42:06 +0200

    include http call

--- internal/controllers/velibs.go
+++ internal/controllers/velibs.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"fmt"
+	"html"
 	"net/http"
 
 	log "github.com/sirupsen/logrus"
@@ -8,11 +10,27 @@ import (
 
 func ReadVelib() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		// vars := mux.Vars(r)
-		// id := vars["id"]
+		lat := "48.853169"
+		long := "2.402782"
+		radius := "100"
+
+		route := "https://opendata.paris.fr/api/records/1.0/search"
+
+		// Assemblage route + query string avec encodage des caractères spéciaux
+		params := fmt.Sprintf("?dataset=velib-disponibilite-en-temps-reel&facet=overflowactivation&facet=creditcard&facet=kioskstate&facet=station_state&geofilter.distance=%s,%s,%s",
+			lat, long, radius)
+		url := fmt.Sprintf("%s/%s", route, html.EscapeString(params))
+
+		_, err := http.Get(url)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
 
 		log.WithFields(log.Fields{
 			//"id": id,
 		}).Info("Reading velib")
+
+		w.WriteHeader(http.StatusOK)
 	}
 }
