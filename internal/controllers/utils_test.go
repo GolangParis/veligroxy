5f57af067005622db72288f5e6e087047049c175
Fabian Gutierrez <fagossa@gmail.com>
2019-10-05 16:05:38 +0200

    add another test case

--- internal/controllers/utils_test.go
+++ internal/controllers/utils_test.go
@@ -1,21 +1,44 @@
 package controllers
 
 import (
+	"reflect"
+	"strings"
 	"testing"
 )
 
-func TestBuildQueryUrl(t *testing.T) {
+func TestBuildQueryUrlWithElements(t *testing.T) {
+	// Given
 	queryParams := map[string][]string{
 		"dataset":            []string{"velib-disponibilite-en-temps-reel"},
 		"facet":              []string{"overflowactivation", "creditcard", "kioskstate", "station_state"},
 		"geofilter.distance": []string{"1,2,3"},
 	}
 
+	// When
 	response := buildQueryParams("?", queryParams)
+	responseTokens := strings.Split(response, "&")
 
+	// Then
 	expected := "?dataset=velib-disponibilite-en-temps-reel&facet=overflowactivation&facet=creditcard&facet=kioskstate&facet=station_state&geofilter.distance=1,2,3"
+	expectedTokens := strings.Split(expected, "&")
 
-	if response != expected {
-		t.Errorf("Query build was incorrect, got: <%s>, want: <%s>", response, expected)
+	if reflect.DeepEqual(responseTokens, expectedTokens) != true {
+		t.Errorf("Query build was incorrect, got: <%s>, want: <%s>", responseTokens, expectedTokens)
+	}
+}
+
+func TestBuildQueryUrlWhenEmpty(t *testing.T) {
+	// Given
+	emptyQueryParams := map[string][]string{}
+
+	// When
+	response := buildQueryParams("?", emptyQueryParams)
+	responseTokens := strings.Split(response, "&")
+
+	// Then
+	expectedTokens := []string{"?"}
+
+	if reflect.DeepEqual(responseTokens, expectedTokens) != true {
+		t.Errorf("Query build was incorrect, got: <%s>, want: <%s>", responseTokens, expectedTokens)
 	}
 }
